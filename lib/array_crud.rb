ede45f1ec3e3c19e7a92bd8d93b3bff3269b306d
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-24 04:07:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,10 +7,10 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  avatar = ["Water", "Earth", "Fire", "Air"]
-  
   avatar << "Blood Bending!"
   puts avatar.inspect
+  
+  avatar 
 end
 
 def add_element_to_start_of_array(array, element)
