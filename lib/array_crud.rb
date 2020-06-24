e8d703ee1e89c73996da083b44d5cb5efad96393
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-24 04:31:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,12 @@ def add_element_to_start_of_array(array, element)
 end 
 
 def remove_element_from_end_of_array(array)
+  avatar = ["Water", "Earth", "Fire", "Air"]
+  Air = avatar.pop
   
+  puts avatar.inspect
+  
+  puts Air
 end
 
 def remove_element_from_start_of_array(array)
